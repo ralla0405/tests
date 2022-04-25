71b57a8b798a07dee03a2bb0856d2ab4612acf74
ralla0405 <ralla0405@gmail.com>
2022-04-25 11:32:36 +0900

    Spring Study
    create Entity and Member repository, service, serviceTest

--- spring/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,16 +17,37 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    // N + 1 문제로 @XToOne 관계는 기본이 즉시로딩이므로 직접 지연로딩으로 설정해야 한다.
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany
+    // cascade 는 persist 전파하는 역할
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
-    private OrderStatus status;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // ORDER, CANCEL
+
+    //== 연관관계 펀의 메서드 ==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
---
+++ spring/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.respository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Rollback(value = false) // 실제 처리가 어떻게 되는지 확인해보기 위해
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class) // 오류가 IllegalStateException 이면 예외처리
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); // 오류가 발생해야한다 (중복처리)
+
+        //then
+        fail("예외가 발생해야 한다.");
+    }
+
+}
