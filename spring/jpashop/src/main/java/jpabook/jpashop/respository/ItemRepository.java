8fb14b37692f00a9f00dd024130b2aa8c03534ab
ralla0405 <ralla0405@gmail.com>
2022-04-26 15:59:10 +0900

    Spring Study
    create Item repository, service, test and Order repository, service, test

---
+++ spring/jpashop/src/main/java/jpabook/jpashop/respository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.respository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            // update 와 비슷
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long itemId) {
+        return em.find(Item.class, itemId);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
