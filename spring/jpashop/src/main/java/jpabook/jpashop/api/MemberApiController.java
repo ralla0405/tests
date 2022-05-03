fd3cf22a92c37fffe0fa65abb0807ce534165bc9
ralla0405 <ralla0405@gmail.com>
2022-05-03 09:16:26 +0900

    Spring Study
    api study

--- spring/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 // @Controller 와 @ResponseBody 를 합친 어노테이션
 @RestController
@@ -15,6 +16,26 @@ import javax.validation.Valid;
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    /**
+     * 조회 V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 기본적으로 엔티티의 모든 값이 노출된다.
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다. (@JsonIgnore, 별도의 뷰 로직 등등)
+     * - 실무에서는 같은 엔티티에 대해 API가 용도에 따라 다양하게 만들어지는데, 한 엔티티에 각각의
+     API를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면 항후 API 스펙을 변경하기 어렵다.(별도의 Result 클래스
+     생성으로 해결)
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO를 반환한다.
+     */
+    // 조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, API가 이거 하나인가! 화면에 종속적이지 마라!
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
 
     // 엔티티 노출은 위험하다
     @PostMapping("/api/v1/members")
@@ -24,7 +45,7 @@ public class MemberApiController {
     }
 
     /**
-     * 도메인 객체를 사용하지 않고 API 설계
+     * DTO 사용하여 API 설계
      * @param request
      * @return
      */
@@ -37,12 +58,19 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /**
+     * DTO 사용하여 도메인회원 수정 API 설계
+     * @param id
+     * @param request
+     * @return
+     */
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(
             @PathVariable("id") Long id,
             @RequestBody @Valid UpdateMemberRequest request) {
-//        memberService.update(id, )
-        return new UpdateMemberResponse(id, request.getName());
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
     @Data
--- spring/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,6 +21,7 @@ public class Member {
     @Embedded
     private Address address;
 
+//    @JsonIgnore 엔티티 조회시 json 반환 무시
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
