77bb2b220757390ef43bd640f25d2caec94fe90d
ralla0405 <ralla0405@gmail.com>
2022-07-14 18:02:27 +0900

    Spring Study
    고급 API 조회 4 5 6

--- spring/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,11 +6,14 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.respository.OrderRepository;
 import jpabook.jpashop.respository.OrderSearch;
+import jpabook.jpashop.respository.order.query.OrderQueryDto;
+import jpabook.jpashop.respository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -22,6 +25,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
@@ -55,13 +59,26 @@ public class OrderApiController {
     }
 
     @GetMapping("/api/v3.1/orders")
-    public List<OrderDto> ordersV3_page() {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
         return result;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
     }
 
     @Data
--- spring/jpashop/src/main/java/jpabook/jpashop/respository/OrderRepository.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/respository/OrderRepository.java
@@ -2,7 +2,6 @@ package jpabook.jpashop.respository;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.respository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -97,6 +96,16 @@ public class OrderRepository {
     TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
     //최대 1000건
     return query.getResultList();
+  }
+
+  public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+    return em.createQuery(
+      "select o from Order o" +
+              " join fetch o.member m" +
+              " join fetch o.delivery d", Order.class)
+      .setFirstResult(offset)
+      .setMaxResults(limit)
+      .getResultList();
   }
 
   public List<Order> findAllWithMemberDelivery() {
